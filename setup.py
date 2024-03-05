12b68024b9b6cc71a610e780cb231d6640577519
Kakolla <kevin.kakolla@fujitsu.com>
2024-03-05 14:57:20 -0500

    initial working changes

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='brain activity classification using recurrent neural networks',
+    author='Kevin Kakolla,Tilisha, Amit',
+    license='MIT',
+)
